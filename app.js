46d91877c4c77c69681a60aceb3bbf8411696716
qori-aziz-kyc <168608559+qori-aziz-kyc@users.noreply.github.com>
2024-12-17 15:30:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-app.use(cors())
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions))
 
 
 // view engine setup
